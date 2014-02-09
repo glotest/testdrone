56f52dcd00d3b21623bb4a73560c1d1c456bd7c5
Paul <paultax@gmail.com>
2014-02-09 19:35:37 +0100

    lkj

--- test.py
+++ test.py
@@ -3,7 +3,7 @@ import unittest
 class MyTest(unittest.TestCase):
 
     def test_add(self):
-        self.assertEqual(1 + 3, 4) 
+        self.assertEqual(2 + 3, 4) 
 
     def test_divide(self):
         self.assertEqual(8 / 2, 5)
