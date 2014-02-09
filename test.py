21c30cc0a328febc56f8345eacea69e8a5fe2515
Paul <paultax@gmail.com>
2014-02-09 17:53:46 +0100

    more tests

--- test.py
+++ test.py
@@ -5,5 +5,8 @@ class MyTest(unittest.TestCase):
     def test_add(self):
         self.assertEqual(1 + 3, 4) 
 
+    def test_divide(self):
+        self.assertEqual(8 / 2, 4) 
+
 if __name__ == '__main__':
     unittest.main()
